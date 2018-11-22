876604fd038ab4fa7a3d11533b13923f12bd14fb
Simone Orsi <simahawk@gmail.com>
2018-11-22 12:53:34 +0100

    Fix NotImplementedError catching

--- git_aggregator/repo.py
+++ git_aggregator/repo.py
@@ -258,9 +258,10 @@ class Repo(object):
             url = url.split(' ')[0]
             v = remotes.setdefault(name, url)
             if v != url:
-                raise NotImplemented(
+                raise NotImplementedError(
                     'Different urls gor push and fetch for remote %s\n'
-                    '%s != %s' % (name, url, v))
+                    '%s != %s' % (name, url, v)
+                )
         return remotes
 
     def _set_remote(self, name, url):
