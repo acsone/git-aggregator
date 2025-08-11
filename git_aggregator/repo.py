e11221e434b6cd5bec0606c4424d647bead0e978
Michael Tietz <mtietz@mt-software.de>
2025-08-11 10:27:38 +0200

    [REF] move log_call in own Class

--- git_aggregator/repo.py
+++ git_aggregator/repo.py
@@ -1,4 +1,5 @@
 # © 2015 ACSONE SA/NV
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPLv3 (http://www.gnu.org/licenses/agpl-3.0-standalone.html)
 # Parts of the code comes from ANYBOX
 # https://github.com/anybox/anybox.recipe.odoo
@@ -10,6 +11,7 @@ import subprocess
 import requests
 
 from ._compat import console_to_str
+from .command import CommandExecutor
 from .exception import DirtyException, GitAggregatorException
 
 FETCH_DEFAULTS = ("depth", "shallow-since", "shallow-exclude")
@@ -30,7 +32,7 @@ def ishex(s):
     return True
 
 
-class Repo:
+class Repo(CommandExecutor):
 
     _git_version = None
 
@@ -56,7 +58,7 @@ class Repo:
         :param bool force:
             When ``False``, it will stop if repo is dirty.
         """
-        self.cwd = cwd
+        super().__init__(cwd)
         self.remotes = remotes
         if fetch_all is True:
             self.fetch_all = frozenset(r["name"] for r in remotes)
@@ -147,21 +149,6 @@ class Repo:
             elif fullref == ref and ref == 'HEAD':
                 return 'HEAD', sha
         return None, ref
-
-    def log_call(self, cmd, callwith=subprocess.check_call,
-                 log_level=logging.DEBUG, **kw):
-        """Wrap a subprocess call with logging
-        :param meth: the calling method to use.
-        """
-        logger.log(log_level, "%s> call %r", self.cwd, cmd)
-        try:
-            ret = callwith(cmd, **kw)
-        except Exception:
-            logger.error("%s> error calling %r", self.cwd, cmd)
-            raise
-        if callwith == subprocess.check_output:
-            ret = console_to_str(ret)
-        return ret
 
     def aggregate(self):
         """ Aggregate all merges into the target branch
